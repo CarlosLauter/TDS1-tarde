0110c2a6ce16d0bc6d0e615d646e8489b0fbefa5
Carlos Lauter <159487795+CarlosLauter@users.noreply.github.com>
2024-09-13 14:15:12 -0300

    Fatorial por loop

---
+++ C/Coding em C/Ztestes/fatorial por loop.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+    int fatorial (int n){
+        int res = 1;
+        
+        for (int i = n; i > 1; i--){
+            res *= i;
+        }
+        return res;
+    
+    }
+int main (){
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+    printf("%d", fatorial(numero)); 
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
