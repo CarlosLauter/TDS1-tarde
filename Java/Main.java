27be852ee37755b47cfa58b128c14f40115064f2
Carlos Lauter <159487795+CarlosLauter@users.noreply.github.com>
2024-10-21 15:54:08 -0300

    Add files via upload

--- Java/Main.java
+++ Java/Main.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = 5, resultado = 0;
-
-        for (int i = 0; i <= 5; i++) {
-            resultado += 5 * (num-1);
+        System.out.println("Digite a quantidade de termos");
+        int n1 = 0, n2 = 1;
+        for (int n = scanner.nextInt(); n > 0; n--) {
+            System.out.print(n1 + " ");
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
         }
-        System.out.println(resultado);
     }
 }
